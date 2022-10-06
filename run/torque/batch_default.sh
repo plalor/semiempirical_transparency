17373a0b663a49f310d52db53301a2ad68d88798
Peter Lalor <peterwlalor@gmail.com>
2022-10-06 14:24:53 -0400

    Trial batch script

--- run/torque/batch_default.sh
+++ run/torque/batch_default.sh
@@ -4,8 +4,6 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=24:00:00
  
-cd /home/plalor/alphaCurveSim/run/torque #<-- change this to your own directory
+cd /home/plalor/alphaCurveSim/run/torque
 
-grasshopper tmp.gdml default.root 0 #make sure you have a directory called torque
-
-#exit 
+grasshopper input_file.gdml output_file.root 0
