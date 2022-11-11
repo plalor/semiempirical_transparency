3036c3a0358017ed3367544b61a1f048d40e1f59
Peter Lalor <peterwlalor@gmail.com>
2022-11-11 12:59:26 -0500

    Gearing up for hi-res and source fluctuation runs

--- run/batch_default.sh
+++ run/batch_default.sh
@@ -2,7 +2,7 @@
  
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=72:00:00
+#SBATCH --time=120:00:00
  
 cd output_dir
 
---
+++ run/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+run_dir="/home/plalor/alphaCurveSim/run"
+source_dir="/home/plalor/alphaCurveSim/src"
+
+for input_file in "$source_dir"/*gdml
+do
+  lmbda=$(expr "$input_file" : '.*lmbda=\([0-9]*\).*')
+  N=$(expr "$input_file" : '.*N=\([0-9]*\).*')
+  E=$(expr "$input_file" : '.*E=\([0-9]*\).*')
+  if [ $lmbda -gt 0 ]
+  then
+    Z=$(expr "$input_file" : '.*Z=\([0-9]*\).*')
+    filebase="E=${E}MeV-lmbda=${lmbda}-Z=${Z}-N=${N}"
+  else
+    filebase="E=${E}MeV-lmbda=${lmbda}-N=${N}"
+  fi
+  output_dir="${run_dir}/torque/${filebase}"
+  output_file="${output_dir}/${filebase}.root"
+  mkdir $output_dir
+  cp ${run_dir}/input_spectrum_${E}MeV.txt ${output_dir}/input_spectrum.txt
+  sed -e "s|output_dir|$output_dir|" -e "s|input_file|$input_file|" -e "s|output_file|$output_file|" $run_dir/batch_default.sh > $output_dir/$filebase.sh
+  
+  cd $output_dir
+  sbatch $filebase.sh
+  sleep 1
+done
